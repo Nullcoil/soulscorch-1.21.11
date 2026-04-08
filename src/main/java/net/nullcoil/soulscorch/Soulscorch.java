7f0cb04ee77df5914fdc096b51832a55c18b84d0
Nullcoil <163534390+Nullcoil@users.noreply.github.com>
2026-04-07 20:12:56 -0400

    Testing a thing

--- src/main/java/net/nullcoil/soulscorch/Soulscorch.java
+++ src/main/java/net/nullcoil/soulscorch/Soulscorch.java
@@ -25,7 +25,7 @@ import org.slf4j.LoggerFactory;
 
 public class Soulscorch implements ModInitializer {
 	public static final String MOD_ID = "soulscorch";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID); //test
 
 	@Override
 	public void onInitialize() {
